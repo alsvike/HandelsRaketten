b6cbc20931f2deb363a61c2a906bff5b03d09d20
alsvike <148956728+alsvike@users.noreply.github.com>
2024-05-07 12:49:43 +0200

    Add project files.

---
+++ HandelsRaketten/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using HandelsRaketten.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HandelsRaketten.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public readonly SignInManager<User> _signInManager;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger, SignInManager<User> signInManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
